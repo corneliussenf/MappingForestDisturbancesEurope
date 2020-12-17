0878f4dd79963d6f028e0bcca6880ecb1a6fffdf
corneliussenf <corneliussenf@gmail.com>
2020-12-17 10:10:59 +0100

    Fixed error in the creation of no-forest plots.

--- lib/01_create_plots_and_extract_predictors.R
+++ lib/01_create_plots_and_extract_predictors.R
@@ -7,50 +7,60 @@ library(sf)
 
 # Functions ---------------------------------------------------------------
 
+# Create plots for non-forest areas
+
 create_plots <- function(cntr, noforest_n) {
   
   ### Get plot data
   
-  plots <- shapefile(list.files("data/plots", glob2rx(paste0("*", cntr, "*.shp")), full.names = TRUE))
+  plots <- read_sf(list.files("data/plots", glob2rx(paste0("*", cntr, "*.shp")), full.names = TRUE))
   
   ### Load forest mask and sample n no-forest pixels
   
   forest_mask <- raster(paste0("data/forestmasks/forestmask_", cntr, ".tif"))
+  
   cntr_shapefile <- read_sf(paste0("data/countries/", cntr, ".shp"))
   
-  forest_mask_sample <- st_sample(cntr_shapefile, noforest_n)
-  forest_mask_sample <- st_as_sf(forest_mask_sample)
-  extr <- raster::extract(forest_mask, forest_mask_sample)
-  forest_mask_sample_noforest <- filter(forest_mask_sample, extr == 0)
+  forest_mask_sample <- sampleRandom(forest_mask, noforest_n, na.rm = TRUE, sp = TRUE)
+  forest_mask_sample_noforest <- subset(forest_mask_sample, forest_mask_sample@data[, 1] == 0)
+  forest_mask_sample_noforest <- st_as_sf(forest_mask_sample_noforest)
+  forest_mask_sample_noforest <- forest_mask_sample_noforest[st_within(forest_mask_sample_noforest, cntr_shapefile, sparse = FALSE)[, 1], ]
   
   while (nrow(forest_mask_sample_noforest) < noforest_n) {
-    forest_mask_sample <- st_sample(cntr_shapefile, (noforest_n - nrow(forest_mask_sample_noforest)) * 2)
-    forest_mask_sample <- st_as_sf(forest_mask_sample)
-    extr <- raster::extract(forest_mask, forest_mask_sample)
-    forest_mask_sample_noforest <- rbind(forest_mask_sample_noforest, filter(forest_mask_sample, extr == 0))
+    forest_mask_sample_tmp <- sampleRandom(forest_mask, noforest_n, na.rm = TRUE, sp = TRUE)
+    forest_mask_sample_noforest_tmp <- subset(forest_mask_sample_tmp, forest_mask_sample_tmp@data[, 1] == 0)
+    forest_mask_sample_noforest_tmp <- st_as_sf(forest_mask_sample_noforest_tmp)
+    forest_mask_sample_noforest_tmp <- forest_mask_sample_noforest_tmp[st_within(forest_mask_sample_noforest_tmp, cntr_shapefile, sparse = FALSE)[, 1], ]
+    forest_mask_sample_noforest <- rbind(forest_mask_sample_noforest, forest_mask_sample_noforest_tmp)
   }
   
   forest_mask_sample_noforest <- forest_mask_sample_noforest[1:noforest_n, ]
   
+  forest_mask_sample_noforest$plotid <- NA
+  forest_mask_sample_noforest <- forest_mask_sample_noforest[, -1]
+  
   # Return
   
-  return(list(st_as_sf(plots), forest_mask_sample_noforest))
+  if ("plotid2" %in% names(plots)) {
+    plots$plotid2 <- NULL
+  }
+  
+  return(list(plots, forest_mask_sample_noforest))
   
 }
 
-extract_landtrendr <- function(cntr, band, plots, landtrednr_path) {
-  
-  print(paste0("...", band))
+# Extract LandTrendr
+
+extract_landtrendr <- function(cntr, band, plots) {
   
   ### Load LandTrendr outputs
   
-  landtrendr_change_images <- list.files(landtredr_path, pattern = glob2rx(paste0("*", cntr, "*", band, "*.tif")), 
-                                         recursive = TRUE, full.names = TRUE) %>%
+  landtrendr_change_images <- list.files("LandTrendr/landtrendr_runs/", pattern = glob2rx(paste0("*", cntr, "*", band, "*.tif")), recursive = TRUE, full.names = TRUE) %>%
     map(stack)
   
   # Extract LandTrendr
   
-  landtrendr_extract_forest <- landtrendr_change_images %>% 
+  landtrendr_extract <- landtrendr_change_images %>% 
     map(~ raster::extract(., plots[[1]])) %>%
     map(~ as.data.frame(.)) %>%
     map(~ setNames(., as.vector(outer(c("year", "magnitude", "duration", "pre", "rate", "dsnr"), band, paste, sep = ".")))) %>%
@@ -58,7 +68,7 @@ extract_landtrendr <- function(cntr, band, plots, landtrednr_path) {
     map(na.omit) %>%
     bind_rows()
   
-  landtrendr_extract_noforest <- landtrendr_change_images %>% 
+  landtrendr_extract_forest <- landtrendr_change_images %>% 
     map(~ raster::extract(., plots[[2]])) %>%
     map(~ as.data.frame(.)) %>%
     map(~ setNames(., as.vector(outer(c("year", "magnitude", "duration", "pre", "rate", "dsnr"), band, paste, sep = ".")))) %>%
@@ -66,13 +76,76 @@ extract_landtrendr <- function(cntr, band, plots, landtrednr_path) {
     map(na.omit) %>%
     bind_rows()
   
-  return(list(landtrendr_extract_forest, landtrendr_extract_noforest))
+  return(list(landtrendr_extract, landtrendr_extract_forest))
   
 }
 
-# Do the extraction -------------------------------------------------------
+# Extract habitat metrics
+
+extract_habitat_metrics <- function(cntr, plots) {
+
+  ### 1984-1986
+
+  habitat_metrics_start <- list.files("LandTrendr/habitat_metrics/", pattern = glob2rx(paste0("*", cntr, "*1984*", "*.tif")), recursive = TRUE, full.names = TRUE) %>%
+    map(stack) %>%
+    map(~ if (nlayers(.) == 14) {subset(., c(2, 8, 11))} else if (nlayers(.) == 3) {subset(., c(2, 1, 3))})
+
+  habitat_metrics_extract_start <- habitat_metrics_start %>%
+    map(~ raster::extract(., plots[[1]])) %>%
+    map(~ as.data.frame(.)) %>%
+    map(~ setNames(., paste(c("TCG", "TCB", "TCW"), "1985", sep = "."))) %>%
+    map(~ mutate(., plotid = plots[[1]]$plotid)) %>%
+    map(na.omit) %>%
+    bind_rows()
+
+  habitat_metrics_extract_start_forest <- habitat_metrics_start %>%
+    map(~ raster::extract(., plots[[2]])) %>%
+    map(~ as.data.frame(.)) %>%
+    map(~ setNames(., paste(c("TCG", "TCB", "TCW"), "1985", sep = "."))) %>%
+    map(~ mutate(., plotid = 99999999)) %>%
+    map(na.omit) %>%
+    bind_rows()
+
+  ### 2016-2018
+
+  habitat_metrics_end <- list.files("LandTrendr/habitat_metrics/", pattern = glob2rx(paste0("*", cntr, "*2018*", "*.tif")), recursive = TRUE, full.names = TRUE) %>%
+    map(stack) %>%
+    map(~ if (nlayers(.) == 14) {subset(., c(2, 8, 11))} else if (nlayers(.) == 3) {subset(., c(2, 1, 3))})
+
+  habitat_metrics_extract_end <- habitat_metrics_end %>%
+    map(~ raster::extract(., plots[[1]])) %>%
+    map(~ as.data.frame(.)) %>%
+    map(~ setNames(., paste(c("TCG", "TCB", "TCW"), "2017", sep = "."))) %>%
+    map(~ mutate(., plotid = plots[[1]]$plotid)) %>%
+    map(na.omit) %>%
+    bind_rows()
+
+  habitat_metrics_extract_end_forest <- habitat_metrics_end %>%
+    map(~ raster::extract(., plots[[2]])) %>%
+    map(~ as.data.frame(.)) %>%
+    map(~ setNames(., paste(c("TCG", "TCB", "TCW"), "2017", sep = "."))) %>%
+    map(~ mutate(., plotid = 99999999)) %>%
+    map(na.omit) %>%
+    bind_rows()
+
+  ### Combine
+
+  habitat_metrics_extract <- cbind(habitat_metrics_extract_start, habitat_metrics_extract_end)
+  habitat_metrics_extract_forest <- cbind(habitat_metrics_extract_start_forest, habitat_metrics_extract_end_forest)
+
+  # habitat_metrics_extract <- habitat_metrics_extract_start
+  # habitat_metrics_extract_forest <- habitat_metrics_extract_start_forest
+
+  return(list(habitat_metrics_extract, habitat_metrics_extract_forest))
+
+}
+
+
+# Create plots ------------------------------------------------------------
 
 samplesizes <- read_delim("data/references/sample_sizes.csv", delim = ";")
+
+plots_out <- vector("list", nrow(samplesizes))
 
 for (i in 1:nrow(samplesizes)) {
   
@@ -84,36 +157,66 @@ for (i in 1:nrow(samplesizes)) {
   
   plots <- create_plots(cntr, noforest_n)
   
-  landtredr_path <- "/media/homedrive/data/Public/LandTrendr" # Path to landtrednr runs (stored in folder with naming 'landtrendr_[country]')
+  plots_combiend <- rbind(plots[[1]], plots[[2]])
   
-  landtrendr_extract <- c("B1", "B2", "B3", "B4", "B5", "B7", "NBR", "TCB", "TCG", "TCW") %>%
-    map(~ extract_landtrendr(cntr, ., plots)) %>% 
-    map(~ set_names(., c("Forest", "Noforest"))) %>%
-    map(bind_rows, .id = "landcover")
+  sf::write_sf(plots_combiend %>% sf::st_as_sf(.), 
+               paste0("data/references/reference_plots/reference_plots_", cntr, ".shp"),
+               overwrite = TRUE)
   
-  plotid <- landtrendr_extract[[1]]$plotid
-  landcover <- landtrendr_extract[[1]]$landcover
+  plots_out[[i]] <- plots_combiend
   
-  landtrendr_extract <- landtrendr_extract %>%
-    map(~ dplyr::select(., -plotid, -landcover)) %>%
-    bind_cols() %>%
-    mutate(., plotid = plotid, landcover = landcover)
+  p <- ggplot() + geom_sf(data = plots_combiend, aes(col = is.na(plotid)))
   
-  coords1 <- st_coordinates(plots[[1]]) %>%
-    as.data.frame %>%
-    set_names(c("x_coord", "y_coord"))
-  
-  coords2 <- st_coordinates(plots[[2]]) %>%
-    as.data.frame %>%
-    set_names(c("x_coord", "y_coord"))
-  
-  coords <- rbind(coords1, coords2)
-  
-  data_extract <- cbind(coords, landtrendr_extract)
-  
-  write_csv(data_extract, paste0("data/model_input/model_input_", cntr, ".csv"))
+  print(p)
   
 }
 
+# Run the extraction ------------------------------------------------------
 
+samplesizes <- read_delim("data/references/sample_sizes.csv", delim = ";")
 
+for (i in 1:nrow(samplesizes)) {
+  
+  cntr <- samplesizes[i, "country_name_short"][[1]]
+  
+  if (cntr %in% not_processed) {
+    
+    print(cntr)
+    
+    noforest_n <- samplesizes[i, "noforest_n"][[1]]
+    
+    plots <- create_plots(cntr, noforest_n)
+    
+    landtrendr_extract <- c("B5", "B7", "NBR", "TCW") %>%
+      map(~ extract_landtrendr(cntr, ., plots)) %>% 
+      map(~ set_names(., c("Forest", "Noforest"))) %>%
+      map(bind_rows, .id = "landcover")
+    
+    plotid <- landtrendr_extract[[1]]$plotid
+    landcover <- landtrendr_extract[[1]]$landcover
+    
+    landtrendr_extract <- landtrendr_extract %>%
+      map(~ dplyr::select(., -plotid, -landcover)) %>%
+      bind_cols() %>%
+      mutate(., plotid = plotid, landcover = landcover)
+    
+    habitat_extract <- extract_habitat_metrics(cntr, plots) %>% 
+      bind_rows(., .id = "landcover")
+    
+    coords1 <- plots[[1]]@coords %>%
+      as.data.frame %>%
+      set_names(c("x_coord", "y_coord"))
+    
+    coords2 <- plots[[2]]@coords %>%
+      as.data.frame %>%
+      set_names(c("x_coord", "y_coord"))
+    
+    coords <- rbind(coords1, coords2)
+    
+    data_extract <- cbind(coords, landtrendr_extract, habitat_extract %>% dplyr::select(., -plotid, -landcover))
+    
+    write_csv(data_extract, paste0("LandTrendr/model_input/model_input_", cntr, ".csv"))
+    
+  }
+  
+}
