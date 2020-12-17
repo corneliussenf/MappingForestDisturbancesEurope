ad0077707408251c43eac8a18eb81531d7fb9979
corneliussenf <corneliussenf@gmail.com>
2020-12-17 10:19:26 +0100

    Small update.

--- lib/04_create_LandTrendr_mosaics.R
+++ lib/04_create_LandTrendr_mosaics.R
@@ -14,6 +14,7 @@ countries <- list.files("data/model_input") %>%
   unlist()
 
 landtredr_path <- "/media/homedrive/data/Public/LandTrendr/greatest_change" # Path to landtrendr runs (stored in folder with naming 'landtrendr_[country]')
+habitat_metrics_path <- "/media/homedrive/data/Public/LandTrendr/habitat_metrics" # Path to habitat metrics (stored in folder with naming 'habitat_metrics_[country]_[year]')
 
 model_inp <- countries %>%
   map(~ read_csv(paste0("data/model_input/model_input_", ., ".csv"))) %>%
@@ -25,6 +26,7 @@ band_indices <- c(paste0("B", c(1:5, 7)), "NBR", "TCB", "TCG", "TCW")
 for (cntr in unique(model_inp$country)) {
   
   dir.create(paste0("data/landtrendr_mosaics/", cntr), showWarnings = FALSE, recursive = TRUE)
+  dir.create(paste0("data/habitat_metrics_mosaics/", cntr), showWarnings = FALSE, recursive = TRUE)
   
   print(paste("Running mosaicing for", cntr))
   
@@ -33,6 +35,8 @@ for (cntr in unique(model_inp$country)) {
   outline <- shapefile(paste0("data/countries/", cntr, ".shp"))
   outline <- gBuffer(outline, 0, byid = TRUE)
   rgdal::writeOGR(outline, "data/countries", cntr, "ESRI Shapefile", overwrite_layer = TRUE)
+  
+  ### LandTrendr
   
   for (b in band_indices) {
     
@@ -65,6 +69,22 @@ for (cntr in unique(model_inp$country)) {
     
   }
   
+  # Habitat Metrics
+  
+  c("1984-1986", "2016-2018") %>%
+    map(~ list.files(habitat_metrics_path, pattern = glob2rx(paste0("*", cntr, "*", ., "*.tif")), recursive = TRUE, full.names = TRUE)) %>%
+    map2(.y = c("1984-1986", "2016-2018"),
+         ~ gdalwarp(srcfile = .,
+                    dstfile = paste0("data/habitat_metrics_mosaics/habitat_metrics_", cntr, "_", .y, ".tif"),
+                    t_srs = "EPSG:3035",
+                    tr = c(30, 30),
+                    output_Raster = FALSE,
+                    overwrite = TRUE,
+                    verbose = TRUE,
+                    cutline = paste0("LandTrendr/countries/", cntr, ".shp"),
+                    crop_to_cutline = TRUE,
+                    dstnodata = "-32768"))
+
 }
 
 
